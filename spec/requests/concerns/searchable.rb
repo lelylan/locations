65c0eeb4827febbbb550c1c686bf7f71882c5651
Andrea Reginato <andrea.reginato@gmail.com>
2012-07-27 18:25:31 +0200

    Added searchable shared examples

---
+++ spec/requests/concerns/searchable.rb
@@ -0,0 +1,13 @@
+require File.expand_path(File.dirname(__FILE__) + '/../acceptance_helper')
+
+shared_examples_for 'searchable' do |key, value|
+
+  let!(:result) { FactoryGirl.create :location, key => value, resource_owner_id: user.id.to_s }
+
+  it 'returns the searched location' do
+    page.driver.get uri, {key => value}.merge(token)
+    contains_location result
+    page.should_not have_content resource[key]
+  end
+end
+
