f51d571f2e8d8e3b2f4b7c8659c0e0f3cb1f528d
Andrea Reginato <andrea.reginato@gmail.com>
2012-11-09 14:00:28 +0100

    Fixed resource id definition on event

--- spec/factories/jobs/event.rb
+++ spec/factories/jobs/event.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :event do
     resource_owner_id { FactoryGirl.create(:user).id }
-    resource_id { FactoryGirl.create(:type).id }
+    resource_id { FactoryGirl.create(:location).id }
     resource 'status'
     event 'update'
     data { JSON.parse('{"json": "ok"}') }
